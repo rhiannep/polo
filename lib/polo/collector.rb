8fe6dd8f89aed2ab5c7e567b21cc8c7e65d0f999
rhiannep <price.rhianne@gmail.com>
2020-04-01 18:32:05 +1300

    .to_sql breaking prepared statements, so to it after

--- lib/polo/collector.rb
+++ lib/polo/collector.rb
@@ -17,10 +17,11 @@ module Polo
       unprepared_statement do
         ActiveSupport::Notifications.subscribed(collector, 'sql.active_record') do
           base_finder = @base_class.includes(@dependency_tree).where(@base_class.primary_key => @id)
-          collect_sql(klass: @base_class, sql: base_finder.to_sql)
           base_finder.to_a
         end
       end
+
+      collect_sql(klass: @base_class, sql: base_finder.to_sql)
 
       @selects.compact.uniq
     end
@@ -38,7 +39,7 @@ module Polo
         sql = payload[:sql]
         if payload[:name] =~ /^HABTM_.* Load$/
           collect_sql(connection: @base_class.connection, sql: sql)
-        elsif payload[:name] =~ /^(.*) Load$/ && payload[:binds].nil?
+        elsif payload[:name] =~ /^(.*) Load$/
           begin
             class_name = $1.constantize
             collect_sql(klass: class_name, sql: sql)
@@ -54,8 +55,8 @@ module Polo
     end
 
     def unprepared_statement
-      if ActiveRecord::Base.connection.respond_to?(:unprepared_statement)
-        ActiveRecord::Base.connection.unprepared_statement do
+      if @base_class.connection.respond_to?(:unprepared_statement)
+        @base_class.connection.unprepared_statement do
           yield
         end
       else
